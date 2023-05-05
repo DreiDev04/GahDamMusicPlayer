eaf36d7096ee69d0a473b3d7d9f21c25eed2fa0e
dreidev_04 <tacujan.andrei@gmail.com>
2023-05-05 14:47:52 +0800

    Added imports

--- src/gahdammusicplayerfinals/FileSelector.java
+++ src/gahdammusicplayerfinals/FileSelector.java
@@ -1,10 +1,13 @@
-
 package gahdammusicplayerfinals;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.media.Media;
 
 public class FileSelector {
-     //Set the URL path to String
+    //Set the URL path to String
+
     static String directoryPath = "C:\\Users\\tacuj\\Documents\\NetBeansProjects\\GahDamMusicPlayerFinals\\src\\musicFolder";
     //Set File Directory
     File directory = new File(directoryPath);
@@ -13,17 +16,37 @@ public class FileSelector {
     //Convert the file list to an array of file names
     String[] fileNames = new String[files.length];
     //Convert the file list to an array of file index
-    int [] fileIndex = new int[files.length];
-    
-    public FileSelector(){
-      //Set the file list
+    int[] fileIndex = new int[files.length];
+
+    public FileSelector() {
+        //Set the file list
         for (int i = 0; i < files.length; i++){
             fileNames[i] = "["+ i +"]" + " " + files[i].getName();
             fileIndex[i] = i;
         }
-        
+
+//        for (File file : files) {
+//            String musicFilePath = file.getAbsolutePath();
+//            Media media = new Media(new File(musicFilePath).toURI().toString());
+//
+//            String musicName = (String) media.getMetadata().get("title");
+//            String artistName = (String) media.getMetadata().get("artist");
+//
+//            // Add the music name and artist to the list
+//            musicList.add(new String[]{musicName, artistName});
+//        }
+//
+//        // Print the list of music names and artists
+//        for (String[] music : musicList) {
+//            System.out.println("Music Name: " + music[0]);
+//            System.out.println("Artist Name: " + music[1]);
+//        }
     }
-    public void showFile(){
+
+    List<String[]> musicList = new ArrayList<>();
+
+    // Iterate over each music file and extract its name and artist
+    public void showFile() {
         for (String element : fileNames) {
             System.out.println(element);
         }
--- src/gahdammusicplayerfinals/Song_ArtistName.java
+++ src/gahdammusicplayerfinals/Song_ArtistName.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gahdammusicplayerfinals;
 
-/**
- *
- * @author tacuj
- */
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Song_ArtistName {
-    
+
+    public static void main(String[] args) {
+        // Specify the directory containing the music files
+        String directoryPath = "C:\\Users\\tacuj\\Documents\\NetBeansProjects\\GahDamMusicPlayerFinals\\src\\musicFolder";
+
+        // Create a File object for the directory
+        File directory = new File(directoryPath);
+
+        // Get a list of all the music files in the directory
+        File[] files = directory.listFiles((dir, name) -> name.endsWith(".mp3"));
+
+        // Create a list to store the music names and artists
+        List<String[]> musicList = new ArrayList<>();
+
+        // Iterate over each music file and extract its name and artist
+        for (File file : files) {
+            String musicFilePath = file.getAbsolutePath();
+            Media media = new Media(new File(musicFilePath).toURI().toString());
+
+            String musicName = (String) media.getMetadata().get("title");
+            String artistName = (String) media.getMetadata().get("artist");
+
+            // Add the music name and artist to the list
+            musicList.add(new String[]{musicName, artistName});
+        }
+
+        // Print the list of music names and artists
+        for (String[] music : musicList) {
+            System.out.println("Music Name: " + music[0]);
+            System.out.println("Artist Name: " + music[1]);
+        }
+    }
 }
